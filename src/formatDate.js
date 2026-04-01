7fd218f27b0bf03afa0fdf3987ddbe6e7c7035d3
Cássia <cassiarsqueiroz@gmail.com>
2026-04-01 19:33:50 -0300

    Solution

--- src/formatDate.js
+++ src/formatDate.js
@@ -8,7 +8,51 @@
  * @returns {string}
  */
 function formatDate(date, fromFormat, toFormat) {
-  // write code here
+  const fromSeparator = fromFormat[3];
+  const fromParts = date.split(fromSeparator);
+
+  if (fromParts.length !== 3) {
+    throw new Error('invalid date');
+  }
+
+  const originEntries = fromFormat
+    .slice(0, 3)
+    .map((pattern, i) => [pattern, fromParts[i]]);
+  const originDateParts = Object.fromEntries(originEntries);
+
+  // calcula fullYear e shortYear a partir dos dados de origem
+  let fullYear;
+
+  if ('YYYY' in originDateParts) {
+    fullYear = originDateParts['YYYY'];
+  } else if ('YY' in originDateParts) {
+    const yy = originDateParts['YY'];
+
+    if (!/^\d{2}$/.test(yy)) {
+      throw new Error('invalid year');
+    }
+    fullYear = (Number(yy) < 30 ? '20' : '19') + yy;
+  }
+
+  const shortYear = fullYear ? fullYear.slice(-2) : undefined;
+
+  const resultParts = toFormat.slice(0, 3).map((token) => {
+    if (token === 'YYYY') {
+      return fullYear;
+    }
+
+    if (token === 'YY') {
+      return shortYear;
+    }
+
+    return originDateParts[token];
+  });
+
+  if (resultParts.includes(undefined)) {
+    throw new Error('invalid date');
+  }
+
+  return resultParts.join(toFormat[3]);
 }
 
 module.exports = formatDate;
